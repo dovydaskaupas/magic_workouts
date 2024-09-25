930c00d74e853e9f01f6b10b696f00152291b00e
d.kaupas <dovydaskaupas@gmail.com>
2024-09-25 19:22:04 +0100

    Integrate the WorkoutCard widget into HomeScreen

--- lib/constants/app_strings.dart
+++ lib/constants/app_strings.dart
@@ -4,4 +4,10 @@ class AppStrings {
   // titles
   static const String appName = "Magic Workouts";
   static const String titleHome = "Home";
+
+  // home widgets
+  static const String homeCardNewTitle = "New Workout";
+  static const String homeCardNewMessage = "Record a new workout";
+  static const String homeCardWorkoutsTitle = "Your Workouts";
+  static const String homeCardWorkoutsMessage = "View your recorded workouts";
 }
--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,30 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:magic_workouts/constants/app_strings.dart';
+import 'package:magic_workouts/constants/ui_properties.dart';
+import 'package:magic_workouts/screens/home/widgets/workout_card.dart';
+import 'package:magic_workouts/widgets/custom_app_bar/custom_app_bar.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required this.title});
-
-  final String title;
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final ThemeData theme = Theme.of(context);
-
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: theme.colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              title,
-              style: theme.textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      appBar: const CustomAppBar(title: AppStrings.titleHome),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          const SizedBox(width: double.maxFinite),
+          WorkoutCard(
+            title: AppStrings.homeCardNewTitle,
+            message: AppStrings.homeCardNewMessage,
+            icon: Icons.fitness_center,
+            onTap: _openNewWorkoutScreen,
+          ),
+          const SizedBox(height: UIProperties.paddingCard),
+          WorkoutCard(
+            title: AppStrings.homeCardWorkoutsTitle,
+            message: AppStrings.homeCardWorkoutsMessage,
+            icon: Icons.history,
+            onTap: _openYourWorkoutsScreen,
+          ),
+        ],
       ),
     );
   }
+
+  void _openNewWorkoutScreen() {}
+
+  void _openYourWorkoutsScreen() {}
 }
