353e73a91ee96190d93299b0e8b6ead3ef81503a
d.kaupas <dovydaskaupas@gmail.com>
2024-09-26 19:43:39 +0100

    Create the WorkoutSetNotifier provider

---
+++ lib/providers/set_notifier_provider/workout_set_notifier_provider.dart
@@ -0,0 +1,24 @@
+import 'package:magic_workouts/models/set/workout_set.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'workout_set_notifier_provider.g.dart';
+
+@riverpod
+class WorkoutSetNotifier extends _$WorkoutSetNotifier {
+  @override
+  WorkoutSet build() {
+    return const WorkoutSet();
+  }
+
+  void setExercise(final String? exercise) {
+    state = state.copyWith(exercise: exercise);
+  }
+
+  void setWeight(final int? weight) {
+    state = state.copyWith(weight: weight);
+  }
+
+  void setRepetitions(final int? repetitions) {
+    state = state.copyWith(repetitions: repetitions);
+  }
+}
---
+++ lib/providers/set_notifier_provider/workout_set_notifier_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'workout_set_notifier_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$workoutSetNotifierHash() =>
+    r'69eac10762b52de930c62c11793b83ee6ecee465';
+
+/// See also [WorkoutSetNotifier].
+@ProviderFor(WorkoutSetNotifier)
+final workoutSetNotifierProvider =
+    AutoDisposeNotifierProvider<WorkoutSetNotifier, WorkoutSet>.internal(
+  WorkoutSetNotifier.new,
+  name: r'workoutSetNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$workoutSetNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$WorkoutSetNotifier = AutoDisposeNotifier<WorkoutSet>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
