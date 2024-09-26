5d02ce956968fff97ff2a862d53aa5c994d5b52c
d.kaupas <dovydaskaupas@gmail.com>
2024-09-26 22:13:42 +0100

    Create the RepetitionsInputField widget

--- lib/screens/new_workout/new_workout_screen.dart
+++ lib/screens/new_workout/new_workout_screen.dart
@@ -1,15 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:magic_workouts/constants/app_input_formatters.dart';
 import 'package:magic_workouts/constants/app_strings.dart';
 import 'package:magic_workouts/constants/ui_properties.dart';
-import 'package:magic_workouts/providers/workout_set_notifier_provider/workout_set_notifier_provider.dart';
 import 'package:magic_workouts/screens/new_workout/widgets/exercise_dropdown_button.dart';
+import 'package:magic_workouts/screens/new_workout/widgets/repetitions_input_field.dart';
 import 'package:magic_workouts/screens/new_workout/widgets/save_workout_button.dart';
 import 'package:magic_workouts/screens/new_workout/widgets/weight_input_field.dart';
 import 'package:magic_workouts/screens/new_workout/widgets/workout_set_list/workout_set_list.dart';
 import 'package:magic_workouts/widgets/custom_app_bar.dart';
-import 'package:magic_workouts/widgets/custom_text_field.dart';
 import 'package:magic_workouts/widgets/scrollable_scaffold.dart';
 
 class NewWorkoutScreen extends ConsumerWidget {
@@ -17,32 +15,21 @@ class NewWorkoutScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return ScrollableScaffold(
-      appBar: const CustomAppBar(title: AppStrings.homeCardNewTitle),
+    return const ScrollableScaffold(
+      appBar: CustomAppBar(title: AppStrings.homeCardNewTitle),
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        const SizedBox(
+        SizedBox(
           height: UIProperties.paddingGeneric,
           width: double.maxFinite,
         ),
-        const ExerciseDropdownButton(),
-        const WeightInputField(),
-        CustomTextField(
-          inputFormatters: AppInputFormatters.digitsOnlyFormatter,
-          labelText: AppStrings.newRepetitions,
-          maxLength: 4,
-          onChanged: (newValue) => _setRepetitions(newValue, ref),
-        ),
-        const SaveWorkoutButton(),
-        const WorkoutSetList(),
-        const SizedBox(width: UIProperties.paddingGeneric),
+        ExerciseDropdownButton(),
+        WeightInputField(),
+        RepetitionsInputField(),
+        SaveWorkoutButton(),
+        WorkoutSetList(),
+        SizedBox(width: UIProperties.paddingGeneric),
       ],
     );
-  }
-
-  void _setRepetitions(final String? newValue, final WidgetRef ref) {
-    final int? repetitions = int.tryParse(newValue ?? "");
-
-    ref.read(workoutSetNotifierProvider.notifier).setRepetitions(repetitions);
   }
 }
---
+++ lib/screens/new_workout/widgets/repetitions_input_field.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:magic_workouts/constants/app_input_formatters.dart';
+import 'package:magic_workouts/constants/app_strings.dart';
+import 'package:magic_workouts/providers/workout_set_notifier_provider/workout_set_notifier_provider.dart';
+import 'package:magic_workouts/widgets/custom_text_field.dart';
+
+class RepetitionsInputField extends ConsumerWidget {
+  const RepetitionsInputField({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final int? repetitions = ref.watch(workoutSetNotifierProvider).repetitions;
+
+    return CustomTextField(
+      value: _formatRepetitionsValue(repetitions),
+      inputFormatters: AppInputFormatters.digitsOnlyFormatter,
+      labelText: AppStrings.newRepetitions,
+      maxLength: 4,
+      onChanged: (newValue) => _setRepetitions(newValue, ref),
+    );
+  }
+
+  String? _formatRepetitionsValue(final int? weight) {
+    if (weight == null) return null;
+
+    return weight.toString();
+  }
+
+  void _setRepetitions(final String? newValue, final WidgetRef ref) {
+    final int? repetitions = int.tryParse(newValue ?? "");
+
+    ref.read(workoutSetNotifierProvider.notifier).setRepetitions(repetitions);
+  }
+}
