7efcbbd5e2e67a1278bebf3804878295e4f9e8f4
d.kaupas <dovydaskaupas@gmail.com>
2024-09-26 11:38:05 +0100

    Create a placeholder for NewWorkoutScreen

---
+++ lib/screens/new_workout/new_workout_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewWorkoutScreen extends StatelessWidget {
+  const NewWorkoutScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("New workout screen"),
+    );
+  }
+}
