8ede5b0a968034887d6e2f7d33e820fccba3d111
d.kaupas <dovydaskaupas@gmail.com>
2024-09-27 19:51:35 +0100

    Add navigation to the NewWorkoutScreen from the WorkoutListItem

--- lib/screens/workout_list/widgets/workout_list/workout_list_item.dart
+++ lib/screens/workout_list/widgets/workout_list/workout_list_item.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:magic_workouts/enums/app_routes.dart';
 import 'package:magic_workouts/models/workout/workout.dart';
+import 'package:magic_workouts/utilities/text_theme_extension.dart';
 
-class WorkoutListItem extends StatelessWidget {
+class WorkoutListItem extends ConsumerWidget {
   const WorkoutListItem({
     super.key,
     required this.index,
@@ -12,11 +16,21 @@ class WorkoutListItem extends StatelessWidget {
   final Workout workout;
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final ThemeData theme = Theme.of(context);
+
     return ListTile(
       title: Text(workout.name ?? "-"),
       subtitle: Text(_formattedDate),
-      leading: Text("${index + 1}"),
+      leading: Text(
+        "${index + 1}",
+        style: theme.textTheme.title.copyWith(fontSize: 16),
+      ),
+      trailing: InkResponse(
+        onTap: () => _deleteWorkout(ref),
+        child: const Icon(Icons.delete),
+      ),
+      onTap: () => _editWorkout(context),
     );
   }
 
@@ -27,4 +41,13 @@ class WorkoutListItem extends StatelessWidget {
 
     return "${date.year}/${date.month}/${date.day} ${date.hour}:${date.minute}";
   }
+
+  void _editWorkout(final BuildContext context) {
+    final String workoutDate = workout.date.toString();
+    final String query = "?workoutDate=$workoutDate";
+
+    context.push("${AppRoutes.newWorkout.path}$query");
+  }
+
+  void _deleteWorkout(final WidgetRef ref) {}
 }
